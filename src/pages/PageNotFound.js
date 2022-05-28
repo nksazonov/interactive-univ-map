3ed7307fdf085c9a15ed677a26734df5d7eaed3b
Sazonov Nikita <actpohabt.ns@gmail.com>
2022-05-28 17:37:44 +0300

    migrate tailwind pageNotFound

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,8 +1,8 @@
 const PageNotFound = () => {
   return (
-    <div className="flex flex-col bg-slate-600">
-      <h3>Error 404:</h3>
-      <h1>Page not found!</h1>
+    <div className="flex flex-col items-center mt-16 font-medium text-slate-800">
+      <h3 className="text-3xl">Error 404:</h3>
+      <h1 className="text-5xl mt-4">Page not found!</h1>
     </div>
   )
 }
